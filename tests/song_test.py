1aeb3fd85e2e057e4d45663e109cfda39636b4f0
Heather King <hkingdevelopment@gmail.com>
2021-10-22 13:01:36 +0100

    song test setup and class created

---
+++ tests/song_test.py
@@ -0,0 +1,11 @@
+import unittest
+from classes.song import Song
+
+class TestSong(unittest.TestCase):
+    
+    def setUp(self):
+        self.song_1 = Song("The Muppet Show Theme")
+        self.song_2 = Song("Mah Na Mah Na")
+        self.song_3 = Song("Together Again")
+        self.song_4 = Song("Happiness Hotel")
+        self.song_5 = Song("It's Not Easy Being Green")
